a52c1d4d23fcd2f28321e7902112b3642f3c2701
Padmore Adusei Ening <padmore27@gmail.com>
2026-01-18 23:01:04 +0000

    Task 2: Implemented login, logout, and registration with templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth import login 
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
@@ -16,3 +18,9 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# This is our Registration "Tool"
+class register(CreateView):
+    form_class = UserCreationForm()
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
