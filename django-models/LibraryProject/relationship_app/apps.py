413eaecb7c0f9a32b987e92a0337cac57a00c5b5
Padmore Adusei Ening <padmore27@gmail.com>
2026-01-19 00:01:05 +0000

    Cleanup: Removed all commented code and refined UserProfile for checker

--- django-models/LibraryProject/relationship_app/apps.py
+++ django-models/LibraryProject/relationship_app/apps.py
@@ -1,11 +1,22 @@
 from django.apps import AppConfig
 
-
 class RelationshipAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'relationship_app'
 
-    def ready(self):
-        import relationship_app.signals
-        # (Only do this if you moved signals to signals.py,
-        # otherwise keep them at the bottom of models.py)
+    # REMOVE OR COMMENT OUT THE CODE BELOW
+    # def ready(self):
+    #     import relationship_app.signals
+
+
+# from django.apps import AppConfig
+
+
+# class RelationshipAppConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'relationship_app'
+
+#     def ready(self):
+#         import relationship_app.signals
+#         # (Only do this if you moved signals to signals.py,
+#         # otherwise keep them at the bottom of models.py)
