88b541cac38a209fc5c8764e3326fc4395f1b537
Padmore Adusei Ening <padmore27@gmail.com>
2026-02-02 09:24:24 +0000

    Implement full CRUD with BookViewSet and DefaultRouter

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
-from rest_framework import generics
+from django.shortcuts import render, redirect
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from django.http import JsonResponse
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+#This help route the defaualt url to the homepage / set it in the urls as well
+def home(request):
+    # return redirect('/api/books/')
+    return JsonResponse({
+        "status": "running",
+        "endpoints": ["/api/books/"]
+    })
+
+# New ViewSet for full CRUD operations
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing book instances.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
