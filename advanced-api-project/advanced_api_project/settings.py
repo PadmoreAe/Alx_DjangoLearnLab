91ac4de026e4b174d4ba4f1db08cb87e4db2c988
Padmore Adusei Ening <padmore27@gmail.com>
2026-02-08 08:33:55 +0000

    Implement Generic Views for Book CRUD with permissions

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -117,3 +117,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+
+
+
+# advanced_api_project/settings.py
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
