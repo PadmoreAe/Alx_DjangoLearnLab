16555e343469232fa07f3ab9af2c0a1fed8700db
Padmore Adusei Ening <padmore27@gmail.com>
2026-02-16 21:59:42 +0000

    Project Setup and User Authentication for a Social Media API

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    
+    # The 'Follow' relationship: A user follows many users, and is followed by many users.
+    followers = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='following',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
