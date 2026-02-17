0b8af794da10cb612c055a7306b29eb9a1eb5e16
Padmore Adusei Ening <padmore27@gmail.com>
2026-02-17 19:40:28 +0000

    Project Setup and User Authentication for a Social Media API

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'bio', 'profile_picture')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    # We want to handle the password separately for security
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'bio', 'profile_picture')
+
+    def create(self, validated_data):
+        # We use create_user to properly hash the password!
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture')
+        )
+        # Automatically create a token for the new user
+        Token.objects.create(user=user)
+        return user
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from .serializers import RegisterSerializer, UserSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class LoginView(ObtainAuthToken):
+    permission_classes = [permissions.AllowAny]
+    # This built-in view handles username/password and returns a token
+    def post(self, request, *args, **kwargs):
+        response = super(LoginView, self).post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        user = User.objects.get(id=token.user_id)
+
+        return Response({
+            'token': token.key,
+            'user': UserSerializer(user).data
+        })
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        # This ensures a user can only see/edit their own profile
+        return self.request.user
