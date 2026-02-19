69e2b5485e38abb7102e48b00acccdc4162e3a9f
Padmore Adusei Ening <padmore27@gmail.com>
2026-02-19 14:32:32 +0000

    Implementing User Follows and Feed Functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, PostFeedView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -8,4 +8,7 @@ router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    #post feeds
+    path('feed/', PostFeedView.as_view(), name='post_feed'),
 ]
+
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions, filters
+from rest_framework import generics, viewsets, permissions, filters
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
@@ -21,3 +23,14 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+#Filter post 
+class PostFeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Filter posts where the author is in the user's following list
+        user = self.request.user
+        following_users = user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
